ed4c08ff92afdcf0f52341e730d0ebbca70cca03
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-08-01 15:55:57 +0300

    add 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Sum a mixed list
+"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Sum a mixed list
+    """
+    return sum(mxd_lst)
